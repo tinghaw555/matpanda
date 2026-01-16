c011c0e08eb5e63b372d878d3f31c1058a4b8610
tinghaw555 <tinghaw345@gmail.com>
2026-01-16 09:47:59 +0800

    run_analysis.py

--- run_analysis.py
+++ run_analysis.py
@@ -11,14 +11,20 @@ df = add_revenue(df)
 
 # Save updated CSV
 df.to_csv('sales_with_revenue.csv', index=False)
-highest= df.loc[df['revenue'].idxmax()]
+
+max_row =df.loc[df['revenue'].idxmax()]
+max_revenue = max_row['revenue']
+product = max_row['product']
+
 # Create and save a simple bar plot
 plt.figure(figsize=(8, 5))
 plt.bar(df['product'], df['revenue'])
 plt.title('Revenue by Product')
 plt.ylabel('Revenue ($)')
+plt.xlabel('Product')
 plt.xticks(rotation=45)
 plt.tight_layout()
 plt.savefig('revenue_plot.png')
 
+print(f'the highest revenue is {max_revenue} and it is {product}')
 print("✅ Done! Check for sales_with_revenue.csv and revenue_plot.png")
